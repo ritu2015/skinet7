2e17024f83d1c234c35fed63061e64a4d637e9d6
LL\ritbishl <ritu.bishla@publicissapient.com>
2024-01-03 15:34:36 +0530

    End of section 5

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,15 +1,17 @@
 using API.Dtos;
+using API.Errors;
 using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Core.Specifications;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/[Controller]")]
-    public class ProductsController : ControllerBase
+    public class ProductsController : BaseApiController
     {
         private readonly IGenericRepository<Product> _productsRepo;
         private readonly IGenericRepository<ProductBrand> _productBrandRepo;
@@ -41,21 +43,15 @@ namespace API.Controllers
             // }).ToList();
         }
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse),StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
         {
             var spec=new ProductWithTypesAndBrandSpecification(id);
             var product= await _productsRepo.GetEntityWithSpec(spec);
+            if(product==null) return NotFound(new ApiResponse(404));
             return _mapper.Map<Product, ProductToReturnDto>(product);
-            // return new ProductToReturnDto
-            // {
-            //     Id=product.Id,
-            //     Name=product.Name,
-            //     Description=product.Description,
-            //     PictureUrl=product.PictureUrl,
-            //     Price=product.Price,
-            //     ProductBrand=product.ProductBrand.Name,
-            //     ProductType=product.ProductType.Name
-            // };
+         
         }
          [HttpGet("types")]
         public async Task<ActionResult<IReadOnlyList<ProductType>>>  GetProductTypes()
--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,9 @@
 
+using API.Extensions;
+using API.Middelware;
 using Core.Interfaces;
 using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,18 +11,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<StoreContext>(opt=>{
-opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddApplicationServices(builder.Configuration);
+//builder.Services.Configure<ApiBehaviourOptions>(options =>
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+app.UseStatusCodePagesWithReExecute("/errors/{0}");
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
